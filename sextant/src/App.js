93cc58867b588ab92cfb4da5d1950af18082e13e
Fowowe Samuel <abimifoluwakso@gmail.com>
2023-01-15 21:24:46 +0100

    task 2

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,16 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
+import Banner from './topBanner';
+import Exhibit from './Exhibit';
 
-function App() {
-  return (
-    <div className="Banner">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <Banner bannerText="Sextant" />
+        <Exhibit name="I am an Exhibit!"></Exhibit>
+        <Exhibit name="I am also an Exhibit!"></Exhibit>
+        <Exhibit name="Don't forget me, I am an Exhibit!"></Exhibit>
+      </div>
+    );
+  }
 }
-
-
-
-
-
 
 export default App;
